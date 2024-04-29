8574bb4fa94dfb300d80957b7cf97f514ec84366
Babajide Ojo <o.babajide@indigenesystems.com>
2024-04-30 00:34:48 +0100

    chore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 "use client"
+
 import type { Metadata } from "next";
 import { Provider } from "react-redux";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { store } from "@/redux/store";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <Provider store={store}>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </Provider>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  </Provider>
   );
 }

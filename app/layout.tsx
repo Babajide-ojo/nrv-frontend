3738cf9c8e516534137a7c853dd06e7fb2b12b37
babajide.ojo <o.babajide@indigenesystems.com>
2024-10-26 20:06:41 +0100

    chore

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <LocalizationProvider dateAdapter={AdapterDayjs}>
     <Provider store={store}>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
       <html lang="en" className="h-full">
         <body className={inter.className}>
